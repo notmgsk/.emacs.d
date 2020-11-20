f33e9ba150797215183194300167874961c7ced5
Mark Skilbeck <markskilbeck@gmail.com>
2020-11-20 12:44:18 +0000

    use pytest

--- lisp/lang/python.el
+++ lisp/lang/python.el
@@ -15,3 +15,17 @@
 (use-package poetry
   :straight t
   :after python)
+
+(use-package pytest
+  :straight t
+  :commands (pytest-one pytest-all)
+  :config
+  (add-to-sl-keymap
+   ;; TODO Testing is common enough that I should have a key that is
+   ;; language-dependent, and will do the right thing. e.g. in
+   ;; python-mode, pop up pytest-one, pytest-all, etc
+   ;;
+   ;; TODO Use (magit) transient mode to present a pop up of available
+   ;; options. Fork https://github.com/ionrock/pytest-el?
+   '(("t o" pytest-one)
+     ("t a" pytest-all))))
